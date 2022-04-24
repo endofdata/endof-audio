e36c0b9e9e2e24b4f969cdcfe5e50a25a1f2c99d
endofdata <endofdata@hotmail.de>
2022-04-24 17:28:46 +0200

    StreamProvider now working with pseudo COM instances

--- Audio.Foundation/StreamProvider.cpp
+++ Audio.Foundation/StreamProvider.cpp
@@ -1,10 +1,28 @@
 #include "pch.h"
 #include "StreamProvider.h"
+#include <ObjectFactory.h>
 
 using namespace Audio::Foundation;
+using namespace Audio::Foundation::Unmanaged;
 
-StreamProvider::StreamProvider(int sampleCount, System::IO::Stream^ wavStream) :
-	SampleContainer(sampleCount),
-	m_stream(wavStream)
+StreamProvider::StreamProvider(int sampleCount, System::IO::Stream^ wavStream) : m_stream(wavStream)
 {
+	m_pContainer = ObjectFactory::CreateSampleContainer(sampleCount);
+	m_pSharer = ObjectFactory::CreateSampleSharer();
+}
+
+StreamProvider::~StreamProvider()
+{
+	m_pContainer->Release();
+	m_pSharer->Release();
+}
+
+ISampleContainer& StreamProvider::get_Container()
+{
+	return *m_pContainer;
+}
+
+ISampleSharer& StreamProvider::get_Sharer()
+{
+	return *m_pSharer;
 }
--- Audio.Foundation/StreamProvider.h
+++ Audio.Foundation/StreamProvider.h
@@ -4,19 +4,31 @@
 #include <SampleSharer.h>
 #include <vcclr.h>
 
-using namespace Audio::Foundation::Unmanaged;
+using namespace Audio::Foundation::Unmanaged::Abstractions;
 
 namespace Audio
 {
 	namespace Foundation
 	{
-		public class StreamProvider : public SampleContainer, public SampleSharer
+		public class StreamProvider
 		{
 		public:
 			StreamProvider(int sampleCount, System::IO::Stream^ fred);
+			virtual ~StreamProvider();
+
+			ISampleContainer& get_Container();
+
+			_declspec(property(get = get_Container)) ISampleContainer& Container;
+
+			ISampleSharer& get_Sharer();
+
+			_declspec(property(get = get_Sharer)) ISampleSharer& Sharer;
 
 		private:
 			gcroot<System::IO::Stream^> m_stream;
+
+			ISampleContainer* m_pContainer;
+			ISampleSharer* m_pSharer;
 		};
 	}
 }
