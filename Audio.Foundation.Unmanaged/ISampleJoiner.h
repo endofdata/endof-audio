5a4e42afacfeca2db011ec89b0715790e5adfb16
endofdata <endofdata@hotmail.de>
2023-01-09 20:41:09 +0100

    ISampleJoiner w/o ISampleReciever methods

--- Audio.Foundation.Unmanaged/ISampleJoiner.h
+++ Audio.Foundation.Unmanaged/ISampleJoiner.h
@@ -3,6 +3,7 @@
 #define WINDOWS_LEAN_AND_MEAN
 #include <Windows.h>
 #include <Audio.Foundation.Unmanaged.h>
+#include <ISampleReceiver.h>
 #include <comdef.h>
 
 namespace Audio
@@ -12,19 +13,13 @@ namespace Audio
 		namespace Unmanaged
 		{
 			namespace Abstractions
-			{
+			{				
 				__interface _AUDIO_FOUNDATION_UNMANAGED_API __declspec(uuid("519e9d40-db0b-42c4-b435-90d7ec67206a")) ISampleJoiner : public IUnknown
 				{
 					_declspec(property(get = get_Target, put = put_Target)) ISampleReceiver* Target;
 
 					ISampleReceiver* get_Target() = 0;
 					void put_Target(ISampleReceiver* value) = 0;
-
-					void Flush() = 0;
-
-					void Send() = 0;
-
-					void MixInput(float* pSourceLeft, float* pSourceRight, float level, float pan) = 0;
 				};
 			}
 		}
