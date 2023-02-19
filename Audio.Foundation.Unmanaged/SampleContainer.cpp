1a80dfa81dc4349fa596615e83b9ebca6b35439b
endofdata <endofdata@hotmail.de>
2023-02-19 19:25:29 +0100

    separate SampleContainerBase is not required anymore

--- Audio.Foundation.Unmanaged/SampleContainer.cpp
+++ Audio.Foundation.Unmanaged/SampleContainer.cpp
@@ -1,15 +1,18 @@
 #include "pch.h"
 #include "SampleContainer.h"
 #include "SampleBuffer.h"
+#include "ObjectFactory.h"
 #include <stdexcept>
 
 using namespace Audio::Foundation::Unmanaged;
 using namespace Audio::Foundation::Unmanaged::Abstractions;
 
 SampleContainer::SampleContainer(int sampleCount, int channelCount) : 
-	SampleContainerBase(sampleCount, channelCount),
+	m_isActive(false),
+	m_sampleCount(0),
 	m_refCount(0)
 {
+	CreateChannels(sampleCount, channelCount);
 }
 
 SampleContainer::~SampleContainer()
@@ -31,5 +34,65 @@ bool SampleContainer::GetInterface(REFIID iid, void** ppvResult)
 		return true;
 	}
 	return false;
+}
+
+void SampleContainer::CreateChannels(int sampleCount, int channelCount)
+{
+	int currentSamples = get_SampleCount();
+	int currentChannels = get_ChannelCount();
+
+	if (sampleCount != currentSamples || channelCount != currentChannels)
+	{
+		if (sampleCount != currentSamples)
+		{
+			m_vecChannels.clear();
+			currentChannels = 0;
+		}
+
+		if (channelCount < currentChannels)
+		{
+			for (int c = channelCount; c < currentChannels; c++)
+			{
+				m_vecChannels.pop_back();
+			}
+		}
+		else
+		{
+			for (int c = currentChannels; c < channelCount; c++)
+			{
+				m_vecChannels.push_back(ObjectFactory::CreateSampleBuffer(sampleCount));
+			}
+		}
+		m_sampleCount = sampleCount;
+	}
+}
+
+int SampleContainer::get_SampleCount()
+{
+	return m_sampleCount;
+}
+
+void SampleContainer::put_SampleCount(int sampleCount)
+{
+	CreateChannels(sampleCount, get_ChannelCount());
+}
+
+int SampleContainer::get_ChannelCount()
+{
+	return (int)m_vecChannels.size();
+}
+
+void SampleContainer::put_ChannelCount(int channelCount)
+{
+	CreateChannels(m_sampleCount, channelCount);
+}
+
+ISampleBufferPtr SampleContainer::get_Channel(int index)
+{
+	if (index < 0 || index >= get_ChannelCount())
+	{
+		throw std::out_of_range("Invalid channel index");
+	}
+	return m_vecChannels.at(index);
 }
 
--- Audio.Foundation.Unmanaged/SampleContainer.h
+++ Audio.Foundation.Unmanaged/SampleContainer.h
@@ -3,7 +3,10 @@
 #include "Audio.Foundation.Unmanaged.h"
 #include "UnknownBase.h"
 #include "ISampleContainer.h"
-#include "SampleContainerBase.h"
+#include "ISampleBuffer.h"
+#include <vector>
+
+using namespace Audio::Foundation::Unmanaged::Abstractions;
 
 namespace Audio
 {
@@ -11,16 +14,31 @@ namespace Audio
 	{
 		namespace Unmanaged
 		{
-			class SampleContainer : public SampleContainerBase
+			class SampleContainer : public ISampleContainer
 			{
 			public:
 				SampleContainer(int sampleCount, int channelCount);
 				virtual ~SampleContainer();
 
+				virtual int get_SampleCount();
+				virtual void put_SampleCount(int sampleCount);
+
+				virtual int get_ChannelCount();
+				virtual void put_ChannelCount(int channelCount);
+
+				virtual ISampleBufferPtr get_Channel(int index);
+
 				DECLARE_IUNKNOWN
 
 			protected:
 				virtual bool GetInterface(REFIID riid, void** pResult);
+
+			private:
+				void CreateChannels(int sampleCount, int channelCount);
+
+				std::vector<ISampleBufferPtr> m_vecChannels;
+				int m_sampleCount;
+				bool m_isActive;
 			};
 		}
 	}
