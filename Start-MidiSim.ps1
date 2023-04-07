c2f6e84f4ac08a0065cd69eaff10519cbad1b835
endofdata <endofdata@hotmail.de>
2023-04-07 19:04:27 +0200

    extend test control script parameters

--- Start-MidiSim.ps1
+++ Start-MidiSim.ps1
@@ -1,18 +1,23 @@
 [CmdletBinding()]
 Param(
 	[Parameter(Mandatory=$False)][Int] $DeviceId = 1,
+	[Parameter(Mandatory=$False)][Int] $RecordingSeconds = 5,
+	[Parameter(Mandatory=$False)][Int] $LoopTimes = 2,
 	[Parameter(Mandatory=$False)][String] $MsgRecord = '644AB0',
-	[Parameter(Mandatory=$False)][String] $MsgStop = '6447B0'
+	[Parameter(Mandatory=$False)][String] $MsgStop = '6447B0',
+	[Parameter(Mandatory=$False)][Switch] $StopNow = $False
 )
 
 $midiTool = 'D:\Gamer I5\Documents\Projects\Rust\miditool\target\debug\miditool.exe'
 $deviceParam = "--device=$DeviceId"
 
-Write-Output "Start recording"
-& $midiTool  @($deviceParam,$MsgRecord)
-Start-Sleep -Seconds 5
-Write-Output "Next part"
-& $midiTool  @($deviceParam,$MsgRecord)
-Start-Sleep -Seconds 5
-Write-Output "Terminating"
+If(!$StopNow) {
+	Write-Output "Send first 'Record'"
+	& $midiTool  @($deviceParam,$MsgRecord)
+	Start-Sleep -Seconds $RecordingSeconds
+	Write-Output "Send second 'Record'"
+	& $midiTool  @($deviceParam,$MsgRecord)
+	Start-Sleep -Seconds $($RecordingSeconds * $LoopTimes)
+}
+Write-Output "Send 'Stop'"
 & $midiTool  @($deviceParam,$MsgStop)
