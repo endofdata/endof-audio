0d85dafb806dd6ca0b5b618d190b70917bf06805
endofdata <endofdata@hotmail.de>
2023-05-21 16:22:46 +0200

    add test for interleaved reading of wav files

--- Test/Test.Audio.Foundation/WaveFileTests.cs
+++ Test/Test.Audio.Foundation/WaveFileTests.cs
@@ -11,7 +11,7 @@ namespace Test.Audio.Foundation
 		[Test]
 		public void WaveFileIO()
 		{
-			var name = Path.Combine(Path.GetTempPath(), $"_{Guid.NewGuid().ToString("D")}.wav");
+			var name = Path.Combine(Path.GetTempPath(), $"_{Guid.NewGuid():D}.wav");
 
 			try
 			{
@@ -63,13 +63,83 @@ namespace Test.Audio.Foundation
 
 					var resultData = new float[resultFormat.SampleCount];
 
-					resultFile.ReadSamples(resultData, 0, resultData.Length);
+					resultFile.ReadSamples(resultData, 0, resultData.Length, deInterleave: false);
 
 					Assert.That(resultData, Is.EqualTo(data), "Data is OK");
 				}
 				finally
 				{
 					resultFile.Close();
+				}
+			}
+			finally
+			{
+				if (File.Exists(name))
+				{
+					File.Delete(name);
+				}
+			}
+		}
+
+		[Test]
+		public void InterleavedRead()
+		{
+			var name = Path.Combine(Path.GetTempPath(), $"_{Guid.NewGuid():D}.wav");
+
+			try
+			{
+				var format = new WaveFormat
+				{
+					Channels = 4,
+					SampleFormat = SampleFormat.IEEEFloat,
+					SampleRate = 48000,
+					SampleSize = sizeof(float)
+				};
+
+				var output = new WaveFile(name, format);
+				var sampleCount = 1024;
+				var frames = sampleCount / format.Channels;
+
+				try
+				{
+					var data = new float[sampleCount];
+					var p = 0;
+
+					for (int f = 0; f < frames; f++)
+					{
+						for (int c = 0; c < format.Channels; c++)
+						{
+							data[p++] = c * 1000 + f;
+						}
+					}
+					output.WriteSamples(data, 0, data.Length);
+				}
+				finally
+				{
+					output.Close();
+				}
+
+				var input = new WaveFile(name);
+
+				try
+				{
+					var result = new float[sampleCount];
+
+					input.ReadSamples(result, 0, sampleCount, deInterleave: true);
+
+					var p = 0;
+
+					for (int c = 0; c < format.Channels; c++)
+					{
+						for (int f = 0; f < frames; f++)
+						{
+							Assert.That(result[p++], Is.EqualTo(c * 1000 + f), "De-interleaved read splits frames and returns channels in sequence.");
+						}
+					}
+				}
+				finally
+				{
+					input.Close();
 				}
 			}
 			finally
