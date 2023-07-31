eb977361ccb0b695e0c261a049de61fd72151662
endofdata <endofdata@hotmail.de>
2023-07-31 19:56:54 +0200

    struggle with assembly loading including native libraries

--- Test/Lupus/App.xaml.cs
+++ Test/Lupus/App.xaml.cs
@@ -1,14 +1,21 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 
 namespace Lupus
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
 		protected override void OnStartup(StartupEventArgs e)
 		{
+			// this seems to be the only way to make .NET find a native dependency of a managed assembly
+			var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var path = Environment.GetEnvironmentVariable("PATH");
+			path += $";{basePath}\\runtimes\\win-x64\\native";
+
+			Environment.SetEnvironmentVariable("PATH", path);
+
 			base.OnStartup(e);
 		}
 
