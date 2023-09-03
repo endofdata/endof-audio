03001ebc3441b7a9c1eafd4eefdf5bacf4f6f484
endofdata <endofdata@hotmail.de>
2023-09-03 22:15:35 +0200

    add unit test for creating MessageBuilder from stream

--- Test/Test.Audio.Osc.Unmanaged/MessageBuilderTests.cpp
+++ Test/Test.Audio.Osc.Unmanaged/MessageBuilderTests.cpp
@@ -21,13 +21,13 @@ namespace TestAudioOscUnmanaged
 
 			builder.AddressPattern = address;
 
-			Assert::AreEqual(24, builder.Size);
+			Assert::AreEqual(24, builder.Size, L"Message size is ok.");
 
 			std::stringstream buffer;
 
 			buffer << builder;
 
-			Assert::IsTrue(std::memcmp("/some/test/address\0\0,\0\0\0", buffer.str().c_str(), builder.Size) == 0);
+			Assert::IsTrue(std::memcmp("/some/test/address\0\0,\0\0\0", buffer.str().c_str(), builder.Size) == 0, L"Formatted message is ok.");
 		}
 
 
@@ -62,7 +62,7 @@ namespace TestAudioOscUnmanaged
 				sizeof(float) + 
 				OscString::GetPaddedStringSize(par3.length()) + 
 				0), 
-				builder.Size);
+				builder.Size, L"Size of formatted message is ok.");
 
 			builder.SetNextParameter(TypeTag::Int32, reinterpret_cast<const void*>(&par1));
 			builder.SetNextParameter(TypeTag::Float32, reinterpret_cast<const void*>(&par2));
@@ -75,7 +75,61 @@ namespace TestAudioOscUnmanaged
 
 			const char* expectedResult = "/some/test/address\0\0,ifsT\0\0\0\x2A\0\0\0\x8f\xc2\xf5\x3d\Something wonderful may happen\0\0";
 
-			Assert::IsTrue(std::memcmp(expectedResult, buffer.str().c_str(), builder.Size) == 0);
+			Assert::IsTrue(std::memcmp(expectedResult, buffer.str().c_str(), builder.Size) == 0, L"Formatted message is ok.");
+		}
+
+		TEST_METHOD(CreateFromStream)
+		{
+			std::stringstream source;
+
+			TypeTag parameterTypes[]
+			{
+				TypeTag::Int32,
+				TypeTag::Float32,
+				TypeTag::String,
+				TypeTag::True
+			};
+
+			source.write("/some/test/address\0\0,ifsT\0\0\0\x2A\0\0\0\x8f\xc2\xf5\x3d\Something wonderful may happen\0\0", 68);
+
+			auto builder = MessageBuilder::Create(source);
+
+			Assert::AreEqual("/some/test/address", builder->AddressPattern, L"Address pattern is ok.");
+			Assert::AreEqual(static_cast<int>(_countof(parameterTypes)), builder->ParameterCount, L"Parameter count is ok.");
+
+			for (int i = 0; i < builder->ParameterCount; i++)
+			{
+				Assert::AreEqual(static_cast<char>(parameterTypes[i]), static_cast<char>(builder->ParameterType[i]), L"Parameter types are ok.");
+
+				TypeTag paramType;
+				const void* paramData;
+				int paramSize;
+
+				builder->GetNextParameter(paramType, paramData, paramSize);
+
+				if (MessageBuilder::IsVariantSize(paramType))
+				{
+					Assert::AreEqual(static_cast<char>(TypeTag::String), static_cast<char>(paramType), L"This is the string parameter.");
+					Assert::AreEqual(30, paramSize, L"Variable parameter size is ok.");
+				}
+				else
+				{
+					Assert::AreEqual(MessageBuilder::GetParameterSize(paramType), paramSize, L"Parameter size is ok.");
+				}
+
+				switch (i)
+				{
+				case 0:
+					Assert::AreEqual(0x2A, *reinterpret_cast<const int*>(paramData), L"Int32 parameter is ok.");
+					break;
+				case 1:
+					Assert::AreEqual(0.12f, *reinterpret_cast<const float*>(paramData), L"Float32 parameter is ok.");
+					break;
+				case 2:
+					Assert::AreEqual(0, std::strcmp("Something wonderful may happen", reinterpret_cast<const char*>(paramData)), L"String parameter is ok.");
+					break;
+				}
+			}
 		}
 
 
